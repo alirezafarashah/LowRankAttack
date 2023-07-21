8ed8a1767324c1b862078a0a18f0a1205961ae6c
alireza <ali.d.f.2012@gmail.com>
2023-07-21 09:34:10 +0800

    debug Ui grad norm

--- train.py
+++ train.py
@@ -100,6 +100,7 @@ def train():
                 loss = F.cross_entropy(output, y) - lambda_1 * reg_term1
                 grad = torch.autograd.grad(loss, Ui)[0]
                 grad = grad.detach()
+                print(torch.norm(grad, p=2))
                 Ui = Ui + u_rate * grad
                 Ui = Ui.detach()
 
