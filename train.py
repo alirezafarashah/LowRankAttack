ea2d0eca56317c278697cb6c9ea713e16c7e4836
alireza <ali.d.f.2012@gmail.com>
2023-07-24 16:33:02 +0800

    debug

--- train.py
+++ train.py
@@ -105,7 +105,8 @@ def train():
                 grad = torch.autograd.grad(loss, Ui)[0]
                 grad = grad.detach()
                 next_Ui = Ui + u_rate * grad
-                next_Ui = clamp(next_Ui.reshape(X.shape), data_utils.lower_limit, data_utils.upper_limit).reshape(-1)
+                next_Ui = clamp(next_Ui.reshape(X.shape), data_utils.lower_limit, data_utils.upper_limit).reshape(
+                    (X.shape[0], d))
                 Ui = next_Ui.detach()
             test_loss, test_acc = evaluate_batch(model, V.detach().clone(), Ui.detach().clone(), X, y)
             print(f"2. test loss after train Ui and before train V: {test_loss}, test acc: {test_acc}")
