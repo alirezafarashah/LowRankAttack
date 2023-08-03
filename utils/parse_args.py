1396b9e2237b84609a389e0a31d53b98bec558dd
alireza <ali.d.f.2012@gmail.com>
2023-08-03 16:15:18 +0800

    add eval_transferability.py

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -31,3 +31,26 @@ def get_args():
     # Optimization args
     parser.add_argument('--inner-steps', default=10, type=int, help='Number of steps to optimize Ui')
     return parser.parse_args()
+
+
+def get_eval_args():
+    parser = argparse.ArgumentParser()
+
+    # Architecture settings
+    parser.add_argument('--dataset', default='CIFAR10', type=str, help='One of: CIFAR10, CIFAR100 or SVHN')
+    parser.add_argument('--architecture', default='PreActResNet18', type=str,
+                        help='One of: resnet18, resnet50, preactresnet18. Default: preactresnet18.')
+
+    parser.add_argument('--batch-size', default=128, type=int)
+    parser.add_argument('--data-dir', default='/path/to/datasets/', type=str)
+
+    # Config paths
+    parser.add_argument('--model-path', default='/kaggle/working/',
+                        type=str, help='Pretrained model path')
+    parser.add_argument('--perturbations-path', default='/kaggle/working/perturbations.pt',
+                        type=str, help='Path of perturbations')
+
+    parser.add_argument('--log-dir', default='/kaggle/working/',
+                        type=str, help='Path to save the tensor V')
+
+    return parser.parse_args()
