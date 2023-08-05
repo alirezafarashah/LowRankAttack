22cb7366fba44eec145544757ec4a2a55175c791
alireza <ali.d.f.2012@gmail.com>
2023-08-06 01:36:40 +0800

    debug evaluation

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -28,7 +28,8 @@ class CIFAR10Utils(object):
         if batch_size_test is None:
             batch_size_test = batch_size
 
-        train_dataset = IndexedCIFAR10Dataset(self.cifar10_mean, self.cifar10_std, dir_, train=True, download=True)
+        train_dataset = IndexedCIFAR10Dataset(self.cifar10_mean, self.cifar10_std, dir_, train=True, download=True,
+                                              random_transform=False)
 
         # storing this data to check on main
         self.img_size = (train_dataset.dataset.data.shape[1], train_dataset.dataset.data.shape[2])
@@ -152,7 +153,8 @@ class CIFAR100Utils(object):
         if batch_size_test is None:
             batch_size_test = batch_size
 
-        train_dataset = IndexedCIFAR100Dataset(self.dset_mean, self.dset_std, dir_, train=True, download=True)
+        train_dataset = IndexedCIFAR100Dataset(self.dset_mean, self.dset_std, dir_, train=True, download=True,
+                                               random_transform=False)
 
         # storing this data to check on main
         self.img_size = (train_dataset.dataset.data.shape[1], train_dataset.dataset.data.shape[2])
