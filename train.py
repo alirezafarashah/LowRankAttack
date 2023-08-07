4c355924d76c9344ef43d685f892b58639157b57
alireza <ali.d.f.2012@gmail.com>
2023-08-08 02:23:27 +0800

    typo

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ def train():
                 V = V.detach()
                 V = V + v_rate * torch.div(V_grad, torch.linalg.vector_norm(V_grad, dim=1).unsqueeze(1))
                 V = fro_projection(V, args.max_fro)
-                V.grad.zerp_()
+                V.grad.zero_()
                 Ui.grad.zero_()
                 V = V.detach()
                 Ui = Ui.detach()
