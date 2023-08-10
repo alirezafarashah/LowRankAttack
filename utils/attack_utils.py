bc3f7e602208312902e2633bd991a2a9dc7759ea
alireza <ali.d.f.2012@gmail.com>
2023-08-10 14:10:43 +0800

    test

--- utils/attack_utils.py
+++ utils/attack_utils.py
@@ -8,7 +8,7 @@ def fro_projection(V, Ui):
     # V_array = V.detach().cpu().numpy()
     # return torch.div(V, max(1, np.linalg.norm(V_array, ord='fro') / d))
     factor = torch.max(torch.linalg.vector_norm(torch.matmul(Ui, V), dim=1))
-    return torch.div(V, max(1, factor))
+    return torch.div(V, max(1, factor/0.5))
 
 
 def clamp(X, lower_limit, upper_limit):
