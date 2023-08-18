e094038d867bfae1338c208ec6b310871fc89ff1
alireza <ali.d.f.2012@gmail.com>
2023-08-18 11:07:00 +0800

    debug

--- eval_transferability.py
+++ eval_transferability.py
@@ -91,7 +91,7 @@ def eval_transferability():
     logger.info(args)
 
     eval_dataset = data_utils.get_eval_dataset(args.data_dir)
-    test_loader, eval_loader = data_utils.get_indexed_loaders(args.data_dir, args.batch_size, valid_size=0)[0:1]
+    test_loader, eval_loader = data_utils.get_indexed_loaders(args.data_dir, args.batch_size, valid_size=0)[0:2]
 
     # Define architecture
     args.num_classes = data_utils.max_label + 1  # Labels start from 0
