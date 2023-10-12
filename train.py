7e9787ccbc5d8e21509fb3e42ec51e88f46925d2
alireza <alirezafarashah@gmail.com>
2023-10-12 20:22:41 +0330

    add architectures

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 
 from architectures.preact_resnet import PreActResNet18
 from architectures.resnet import ResNet50, ResNet18
+from architectures.models import ResNet20, ResNet56, VGG16
 from architectures.vgg16 import VGG16
 from architectures.wide_resnet import Wide_ResNet
 
@@ -58,15 +59,19 @@ def test():
         model = PreActResNet18(num_classes=args.num_classes).cuda()
     elif args.architecture.upper() in 'RESNET18':
         model = ResNet18(num_classes=args.num_classes).cuda()
+    elif args.architecture.upper() in 'RESNET20':
+        model = ResNet20().cuda()
     elif args.architecture.upper() in 'RESNET50':
         model = ResNet50(num_classes=args.num_classes).cuda()
+    elif args.architecture.upper() in 'RESNET56':
+        model = ResNet56().cuda()
     elif args.architecture.upper() in 'VGG16':
         model = VGG16().cuda()
     else:
         raise ValueError('Unknown architecture.')
 
     model_path = args.model_path
-    if args.architecture.upper() not in 'VGG16':
+    if args.architecture.upper() not in ['VGG16', 'RESNET20', 'RESNET56']:
         if not os.path.exists(model_path):
             raise ValueError('Pretrained model does not exist.')
         model.load_state_dict(torch.load(model_path))
