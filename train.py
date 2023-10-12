15ae72e968c3c3ef02f3d41b3744ef5ff91df2e6
alireza <alirezafarashah@gmail.com>
2023-10-12 23:42:16 +0330

    add architectures

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ def train():
     logger.info("Pretrained model loaded successfully.")
     print("Pretrained model loaded successfully.")
     model.eval()
-    test_loss, test_acc = evaluate_model(model, test_loader)
+    test_loss, test_acc = evaluate_model(model, eval_loader)
     logger.info(f"Evaluate model on clean dataset, test loss: {test_loss}, test acc: {test_acc}")
     print(f"Evaluate model on clean dataset, test loss: {test_loss}, test acc: {test_acc}")
 
